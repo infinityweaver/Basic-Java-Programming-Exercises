c488c2441e281844689159c20bbb7f0f7c5c3322
KaelNierras <kyleanthony47@gmail.com>
2022-07-06 15:08:53 +0800

    Nierras Kyle Anthony ProgExer5

--- ProgExer1 - Prime Numbers/Prime.java
+++ ProgExer1 - Prime Numbers/Prime.java
@@ -1,32 +1,33 @@
 public class Prime  
 {  
-public static void main(String[] args)   
+
+    public static void main(String[] args)   
     {  
-int ct=0,n=0,i=1,j=1,count=0;  
+        int ct=0,n=0,i=1,j=1,count=0;  
 
-while(n<25)  
-{  
-    j=1;  
-    ct=0;  
+        while(n<25)  
+        {  
+            j=1;  
+            ct=0;  
 
-while(j<=i)  
-{  
-    if(i%j==0)  
-        ct++;  
-        j++;   
-}  
+        while(j<=i)  
+        {  
+            if(i%j==0)  
+                ct++;  
+                j++;   
+        }  
 
-if(ct==2)  
-{  
-    System.out.printf("%d ",i);  
-    count++;
-              if(count == 5) {
-                  System.out.print("\n");
-                  count = 0;
-              }
-    n++;  
-}  
-i++;
-}    
-}  
+        if(ct==2)  
+        {  
+            System.out.printf("%d \t",i);  
+            count++;
+                      if(count == 5) {
+                          System.out.print("\n");
+                          count = 0;
+                      }
+            n++;  
+        }  
+        i++;
+        }    
+    }  
 }  
