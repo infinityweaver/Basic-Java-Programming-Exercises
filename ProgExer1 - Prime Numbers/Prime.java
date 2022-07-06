a670c814734831f6c86a4e8cdd109eb96b6f2275
KaelNierras <kyleanthony47@gmail.com>
2022-07-06 15:00:58 +0800

    Answer ProgExer1

---
+++ ProgExer1 - Prime Numbers/Prime.java
@@ -0,0 +1,32 @@
+public class Prime  
+{  
+public static void main(String[] args)   
+    {  
+int ct=0,n=0,i=1,j=1,count=0;  
+
+while(n<25)  
+{  
+    j=1;  
+    ct=0;  
+
+while(j<=i)  
+{  
+    if(i%j==0)  
+        ct++;  
+        j++;   
+}  
+
+if(ct==2)  
+{  
+    System.out.printf("%d ",i);  
+    count++;
+              if(count == 5) {
+                  System.out.print("\n");
+                  count = 0;
+              }
+    n++;  
+}  
+i++;
+}    
+}  
+}  
