87003084a5e5827c6fc3011e2b2190424f607895
KaelNierras <kyleanthony47@gmail.com>
2022-07-12 13:45:30 +0800

    TimeC.java

--- ProgExer8 - Time Conversion/TimeC.java
+++ ProgExer8 - Time Conversion/TimeC.java
@@ -1,48 +1,47 @@
 import java.util.Scanner;
+
 public class TimeC
 {
-    static void convert(String str) 
+    public static void main(String[] args)
     {
-        // Get Hours
-        int h1 = (int) str.charAt(0) - '0';
-        int h2 = (int) str.charAt(1) - '0';
-        int hh = h1 * 10 + h2;
-        // check if the time should be in AM or PM
-        String mrdn;
-        if (hh < 12)
-        {
-            mrdn = "AM";
-        } else
-            mrdn = "PM";
-        hh %= 12;
-        // Handle 00 and 12 case separately
-        if (hh == 0)
-        {
-            System.out.print("12");
-            // Printing minutes and seconds
-            for (int i = 2; i < 8; ++i) 
-            {
-                System.out.print(str.charAt(i));
-            }
-        }
-        else
-        {
-            System.out.print(hh);
-            // Printing minutes and seconds
-            for (int i = 2; i < 8; ++i) {
-                System.out.print(str.charAt(i));
-            }
-        }
-        // After time is printed print Meridien
-        System.out.println(" " + mrdn);
-    }
-    public static void main(String ar[])
-    {
-        // 24 hour format
-        // The Format of the Input must be HH:MM:SS
-        System.out.print("The Format of the Input must be HH:MM:SS\n");
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        convert(str);
+        String proceed;
+        
+        do
+        {
+
+            System.out.print("Enter time in 24-hr format (hh mm): ");
+            String inputTime = scan.nextLine();
+
+            String[] time = inputTime.split(" ");
+
+            int hours = Integer.parseInt(time[0]);
+            String mins = time[1];
+            String dayTime;
+
+            if(hours < 12)
+            {
+                dayTime = "AM";
+            }
+            else
+            {
+                dayTime = "PM";
+            }
+            hours %= 12;
+
+
+            if(hours == 0) 
+            {
+                System.out.println("It's 12:" + " " + mins +" " + dayTime);
+            }
+            else
+            {
+                System.out.println("It's " + hours + ":" + mins +" " + dayTime);
+            }
+
+            System.out.print("\n" + "Continue to input another time? [y|n]: " );
+            proceed = scan.nextLine();
+
+        }while(proceed.equals("y"));
     }
 }
